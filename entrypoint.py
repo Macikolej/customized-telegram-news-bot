90f4b0f3086b5809438d845def65645f43294cf5
Macikolej <oiejnik152@gmail.com>
2022-06-21 22:03:38 +0200

    fixes on CI

--- entrypoint.py
+++ entrypoint.py
@@ -1,14 +1,13 @@
-import dotenv
 import os
 import subprocess
-
-dotenv.load_dotenv()
 
 MODE = os.getenv("MODE")
 
 if (MODE == "bot"):
-	popen = subprocess.Popen('gunicorn app:app -b :8000', shell = True)
+	popen = subprocess.Popen('gunicorn app:app -b :8000', shell=True)
+	popen.wait()
 elif (MODE == "workers"):
-	popen = subprocess.Popen('python3 scraper.py', shell = True)
+	popen = subprocess.Popen('python3 scraper.py', shell=True)
+	popen.wait()
 else:
-	popen = subprocess.Popen('echo "internal error - wrong environmental variable"', shell = True)
+	popen = subprocess.Popen('echo "internal error - wrong environmental variable"', shell=True)
