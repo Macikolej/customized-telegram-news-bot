058f76b20e455fcecad3e6122c82e1ff6ecb5932
Macikolej <oiejnik152@gmail.com>
2022-06-22 13:17:50 +0200

    configuring bot

--- app.py
+++ app.py
@@ -82,16 +82,16 @@ def respond():
             if (upvote_threshold > 0):
                 try:
                     # reddit.subreddits.search_by_name("leagueoflegends", exact=True)
-                    c.execute(f"""
-                        IF NOT EXISTS (SELECT * FROM subscriptions)
-                                WHERE chat_id="{chat_id}" AND subreddit_name="{subreddit}"
-                        BEGIN
-                            INSERT INTO subscriptions
-                            (id, chat_id, subreddit_name, date_of_subscription, upvotes_threshold)
-                            VALUES ("1", "{chat_id}", "{subreddit}", "{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}", {upvote_threshold})
-                        END
-                    """)
-                    connection.commit()
+                    # c.execute(f"""
+                    #     IF NOT EXISTS (SELECT * FROM subscriptions)
+                    #             WHERE chat_id="{chat_id}" AND subreddit_name="{subreddit}"
+                    #     BEGIN
+                    #         INSERT INTO subscriptions
+                    #         (id, chat_id, subreddit_name, date_of_subscription, upvotes_threshold)
+                    #         VALUES ("1", "{chat_id}", "{subreddit}", "{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}", {upvote_threshold})
+                    #     END
+                    # """)
+                    # connection.commit()
                     bot.sendMessage(chat_id=chat_id, text=f"Subscribed to {subreddit}!", reply_to_message_id=msg_id)
                 except NotFound:
                     bot.sendMessage(chat_id=chat_id, text=f"Subreddit {subreddit} wasn't found!", reply_to_message_id=msg_id)
