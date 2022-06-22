b94ad16b771dfddc7bfa14761fa5495d184baae5
Macikolej <oiejnik152@gmail.com>
2022-06-22 18:28:16 +0200

    configuring bot

--- cleaning_worker.py
+++ cleaning_worker.py
@@ -2,25 +2,13 @@ import dotenv
 import os
 import MySQLdb
 import datetime
-
-dotenv.load_dotenv()
-
-connection = MySQLdb.connect(
-  host=os.getenv("DB_HOST"),
-  user=os.getenv("DB_USERNAME"),
-  passwd=os.getenv("DB_PASSWORD"),
-  db=os.getenv("DB_NAME"),
-  ssl={
-    "ca": "/etc/ssl/cert.pem"
-  })
-
-cursor = connection.cursor()
+import settings
 
 month_ago = datetime.datetime.strptime((datetime.datetime.now() - datetime.timedelta(days = 31)).strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
-cursor.execute(f"""
+settings.cursor.execute(f"""
   DELETE FROM sent WHERE
   post_date < "{month_ago}"
 """)
 
-connection.commit()
+settings.connection.commit()
 
--- scraping_worker.py
+++ scraping_worker.py
@@ -6,36 +6,15 @@ import time
 import datetime
 import telegram
 from prawcore import NotFound
+import settings
 
-dotenv.load_dotenv()
+settings.init()
 
-BOT_API_KEY = os.getenv("BOT_API_KEY")
-bot = telegram.Bot(token=BOT_API_KEY)
-
-reddit = praw.Reddit(
-    client_id=os.getenv("REDDIT_API_ID"),
-    client_secret=os.getenv("REDDIT_API_KEY"),
-    user_agent="ALPINE:123:1.0 (by u/Macikolej)",
-)
-
-
-connection = MySQLdb.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USERNAME"),
-    passwd=os.getenv("DB_PASSWORD"),
-    db=os.getenv("DB_NAME"),
-    ssl={
-    "ca": "/etc/ssl/cert.pem"
-    }
-)
-
-c = connection.cursor()
-
-c.execute(f"""
+settings.cursor.execute(f"""
     SELECT * FROM subscriptions;
 """)
 
-subscriptions = c.fetchall()
+subscriptions = settings.cursor.fetchall()
 
 subreddits = {}
 
@@ -49,7 +28,7 @@ for subscription in subscriptions:
         subreddits[subreddit_name].append(subscriber_tuple)
 
 for subreddit_name in subreddits:
-    subreddit_posts = list(reddit.subreddit(subreddit_name).top(time_filter="month"))
+    subreddit_posts = list(settings.reddit.subreddit(subreddit_name).top(time_filter="month"))
     subscribers = subreddits[subreddit_name]
 
     for subscriber_tuple in subscribers:
@@ -59,10 +38,10 @@ for subreddit_name in subreddits:
             # temp = date_of_subscription - datetime.timedelta(days = 15)
 
             if (subreddit_post.score >= upvotes_threshold and creation_date >= date_of_subscription):
-                c.execute(f"""
+                settings.cursor.execute(f"""
                     SELECT * FROM sent WHERE chat_id="{chat_id}";
                 """)
-                sent_posts = c.fetchall()
+                sent_posts = settings.cursor.fetchall()
                 found = False
                 for sent in sent_posts:
                     sent_chat_id, sent_post_url, _ = sent
@@ -70,9 +49,9 @@ for subreddit_name in subreddits:
                         found = True
 
                 if (not found):
-                    bot.sendMessage(chat_id=chat_id, text=f"reddit.com/{subreddit_post.permalink}")
-                    c.execute(f"""
+                    settings.bot.sendMessage(chat_id=chat_id, text=f"reddit.com/{subreddit_post.permalink}")
+                    settings.cursor.execute(f"""
                         INSERT INTO sent (chat_id, post_url, post_date) VALUES("{chat_id}", "{subreddit_post.permalink}", "{creation_date.strftime('%Y-%m-%d %H:%M:%S')}");
                     """)
 
-connection.commit()
+settings.connection.commit()
